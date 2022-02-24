5b8946128d7b34c312d3293b82dc819751ef1480
Ivon Badra <ibadra.c@gmail.com>
2022-02-23 23:08:12 -0500

    Navbar II

--- scripts.js
+++ scripts.js
@@ -1,13 +1,46 @@
 var myNav = document.getElementById("nav");
 
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
 window.onscroll = function () {
   "use strict";
-  if (
-    document.body.scrollTop >= 700 ||
-    document.documentElement.scrollTop >= 700
-  ) {
-    myNav.classList.add("scroll");
+  if (viewportWidth >= 1024) {
+    if (
+      document.body.scrollTop >= 700 ||
+      document.documentElement.scrollTop >= 700
+    ) {
+      myNav.classList.add("scroll");
+    } else {
+      myNav.classList.remove("scroll");
+    }
   } else {
-    myNav.classList.remove("scroll");
+    if (
+      document.body.scrollTop >= 280 ||
+      document.documentElement.scrollTop >= 280
+    ) {
+      myNav.classList.add("scroll");
+    } else {
+      myNav.classList.remove("scroll");
+    }
   }
 };
+
+// if (viewportWidth >= 1024) {
+//   if (
+//     document.body.scrollTop >= 700 ||
+//     document.documentElement.scrollTop >= 700
+//   ) {
+//     myNav.classList.add("scroll");
+//   } else {
+//     myNav.classList.remove("scroll");
+//   }
+// } else {
+//   if (
+//     document.body.scrollTop >= 280 ||
+//     document.documentElement.scrollTop >= 280
+//   ) {
+//     myNav.classList.add("scroll");
+//   } else {
+//     myNav.classList.remove("scroll");
+//   }
+// }
